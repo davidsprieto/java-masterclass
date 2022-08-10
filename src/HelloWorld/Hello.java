0267978b53ba2bd23af5e65c3ef68cef1893ac80
David Prieto <david.s.prieto1@gmail.com>
2022-08-09 20:12:11 -0500

    Started Udemy Java Course

---
+++ src/HelloWorld/Hello.java
@@ -0,0 +1,12 @@
+package HelloWorld;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, David");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        System.out.println(myFirstNumber);
+    }
+
+}
