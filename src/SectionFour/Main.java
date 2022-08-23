17234cdadbf57218bf8c97a95f08a63d343044ba
David Prieto <david.s.prieto1@gmail.com>
2022-08-23 11:37:36 -0500

    Completed Section 4 - #45

--- src/SectionFour/Main.java
+++ src/SectionFour/Main.java
@@ -5,6 +5,25 @@ public class Main {
     public static void main(String[] args) {
 
 
+        // 08/23/22
+        // Keywords and Expressions:
+        // one mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344); // the expression is 'kilometers = (100 * 1.609344)' - the expression doesn't include the data type pr semicolon (variables, values, and operators make an expression).
+        int highScore = 50;
+
+        if (highScore == 50) {
+            System.out.println("'highScore == 50' is an expression."); // The string in the System.out.println is an expression.
+        }
+
+        // In the following code, write down which parts of the code that are expressions:
+        int score = 100; // 'score = 100'
+        if (score > 99) { // 'score > 99'
+            System.out.println("You got the high score!"); // 'You got the high score!'
+            score = 0; // 'score = 0'
+        }
+
+        // Statements:
+
 
     }
 
