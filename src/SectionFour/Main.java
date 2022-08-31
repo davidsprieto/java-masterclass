166329fddbf26a3b1ee70d7b544a0ccc3c1ccfa2
David Prieto <david.s.prieto1@gmail.com>
2022-08-31 14:19:07 -0500

    Refactor

--- src/SectionFour/Main.java
+++ src/SectionFour/Main.java
@@ -132,9 +132,9 @@ public class Main {
     public static int calculateHighScorePosition(int playerScore) {
         if (playerScore >= 1000) {
             return 1;
-        } else if (playerScore >= 500 && playerScore <= 999) {
+        } else if (playerScore >= 500) {
             return 2;
-        } else if (playerScore >= 100 && playerScore <= 499) {
+        } else if (playerScore >= 100) {
             return 3;
         } else {
             return 4;
