944c8140145744f6c8e7454de86dc8024a3146e6
David Prieto <david.s.prieto1@gmail.com>
2022-09-01 13:03:54 -0500

    Completed Section 4 - Coding Exercise #1

---
+++ src/SectionFour/SpeedConverter.java
@@ -0,0 +1,33 @@
+package SectionFour;
+
+public class SpeedConverter {
+
+    public static void main(String[] args) {
+        printConversion(1.5);
+        printConversion(10.5);
+        printConversion(-5.6);
+        printConversion(25.42);
+        printConversion(75.114);
+
+        System.out.println(toMilesPerHour(1.5));
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return -1;
+        } else {
+            double milesPerHour = (kilometersPerHour / 1.609);
+            return Math.round(milesPerHour);
+        }
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+        }
+    }
+
+}
