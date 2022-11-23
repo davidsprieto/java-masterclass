bf6f6c640a3034bbf491d9ca1d2654030724df15
David Prieto <david.s.prieto1@gmail.com>
2022-11-23 14:47:13 -0600

    Completed print factors algo.

---
+++ src/SectionFive/FactorPrinter.java
@@ -0,0 +1,27 @@
+package SectionFive;
+
+public class FactorPrinter {
+
+    public static void main(String[] args) {
+        printFactors(6);
+        printFactors(32);
+        printFactors(10);
+        printFactors(-1);
+    }
+
+    public static void printFactors(int number) {
+        if (number < 1) {
+            System.out.println("Invalid Value");
+        } else {
+            int count = 1;
+            while (count <= number) {
+                if (number % count == 0) {
+                    System.out.print(count + " ");
+                }
+                count++;
+            }
+            System.out.println("");
+        }
+    }
+
+}
