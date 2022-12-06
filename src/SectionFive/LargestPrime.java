6587adef46baa6b53a000a6e49cf3c01cb572e07
David Prieto <david.s.prieto1@gmail.com>
2022-12-06 13:02:36 -0600

    Completed largest prime and print star algos

---
+++ src/SectionFive/LargestPrime.java
@@ -0,0 +1,28 @@
+package SectionFive;
+
+public class LargestPrime {
+
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(21));
+        System.out.println(getLargestPrime(217));
+        System.out.println(getLargestPrime(0));
+        System.out.println(getLargestPrime(45));
+        System.out.println(getLargestPrime(-1));
+    }
+
+    public static int getLargestPrime(int number) {
+        if (number < 2) {
+            return -1;
+        }
+        int largest = 2;
+        while (number > largest) {
+            if (number % largest == 0) {
+                number /= largest;
+            } else {
+                largest++;
+            }
+        }
+        return largest;
+    }
+
+}
