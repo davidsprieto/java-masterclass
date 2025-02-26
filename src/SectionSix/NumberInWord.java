4e3d87be75978d0e936dc6515f86c1223a68a863
David Prieto <david.s.prieto1@gmail.com>
2025-02-26 16:12:11 -0600

    continuing section 6

--- src/SectionSix/NumberInWord.java
+++ src/SectionSix/NumberInWord.java
@@ -1,9 +1,13 @@
 package SectionSix;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class NumberInWord {
 
     public static void main(String[] args) {
-        printNumberInWord(1);
+        printNumberInWord(4);
+        printNumberInWordRefactor(4);
     }
 
     public static void printNumberInWord(int number) {
@@ -42,5 +46,23 @@ public class NumberInWord {
                 System.out.println("OTHER");
         }
     }
+
+    // REFACTOR
+    public static void printNumberInWordRefactor(int number) {
+        Map<Integer, String> hash = new HashMap<>();
+        hash.put(0, "ZERO");
+        hash.put(1, "ONE");
+        hash.put(2, "TWO");
+        hash.put(3, "THREE");
+        hash.put(4, "FOUR");
+        hash.put(5, "FIVE");
+        hash.put(6, "SIX");
+        hash.put(7, "SEVEN");
+        hash.put(8, "EIGHT");
+        hash.put(9, "NINE");
+
+        System.out.println(hash.getOrDefault(number, "OTHER"));
+    }
+
 
 }
